6baa21d91bc4e57fad20f9b9b57a52863c859f1d
Sanbox labs <definebooks@gmail.com>
2021-10-02 19:46:43 -0400

    project-1-SL-AS

---
+++ JS/project1.js
@@ -0,0 +1,9 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', ()) 
+{
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});
+
+searchButton.onclick = null;
