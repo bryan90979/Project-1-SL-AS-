5ad681cc408c529fd0356cc38050a473e0872d63
Sanbox labs <definebooks@gmail.com>
2021-10-03 16:47:45 -0400

    Project-1-SL-AS

--- JS/project1.js
+++ JS/project1.js
@@ -7,3 +7,24 @@ searchButton.addEventListener('click', ())
 });
 
 searchButton.onclick = null;
+
+var section = document.querySelectorAll ('section')
+var sections = {}
+section.forEach (function(e){
+  console.log (e.id.e.offsetTop)
+  section[e.id] = e.offsetTop; 
+}
+
+window.onscroll- function(){
+  var scrollPos = document.documentElement.scrollTop
+  console.log(scrollPos)
+  for (var i in sections){
+    console.log (section[i]){
+      let (i=0; i++; i<scrollPos.lenght);
+      if (section [i] < scrollPos.lenght)
+      document.querySelector('.active').setAttribute('class',''); 
+      document.querySelector(ahref).setAttribute('class', 'active')
+
+    }
+  }
+}
